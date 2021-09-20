40431f8815730d034ce77bc1656e865f95431e46
Jesse De Lira <jessedelira2000@gmail.com>
2021-09-19 23:29:12 -0400

    feat: added testing for edge cases

--- src/movePicker.py
+++ src/movePicker.py
@@ -1,74 +1,76 @@
 #!/usr/bin/python
+import random as rd
 class movePicker:
+    
+    used_directions = {"Up Right": 0,
+                           "Down Right": 0, "Up Left": 0, "Down Left": 0, "Vertical Above":0}
     @classmethod
-    def get_next_move(self,board):
+    def get_next_move(self, board):
         legal_moves = self.get_all_legal_moves(board)
         return self.get_best_move(legal_moves)
-        
-      
-   
-    @staticmethod
-    def get_all_legal_moves(board):
+
+    @classmethod
+    def get_all_legal_moves(self,board):
         all_legal_moves = []
-        
+
         for row_idx in range(len(board)-1):
             for column_idx in range(len(board[row_idx])-1):
                 possible_points = 1
-                # check if there is a one if a spot
+                # check if there is a one a spot
                 if(board[row_idx][column_idx] == 1):
-                    
+
                     # Horizontal Right
                     if(board[row_idx][column_idx+1] == 2):
-                        for idx in range(column_idx+2,len(board[row_idx])):  
+                        for idx in range(column_idx+2, len(board[row_idx])):
                             if(board[row_idx][idx] == 1):
                                 break
-                            # checking if more than one 2, then adding
                             if(board[row_idx][idx] == 2):
                                 possible_points += 1
                             if(board[row_idx][idx] == 0):
-                                all_legal_moves.append([row_idx,idx,possible_points])
-                                print('all legal moves are:',all_legal_moves)
-                                break       
-                            
+                                all_legal_moves.append([row_idx, idx, possible_points])
+                                print('all legal moves are:', all_legal_moves)
+                                break
+
                     # Horizontal Left
                     if(board[row_idx][column_idx-1] == 2):
-                        for idx in reversed(range(0,column_idx-1)):
+                        for idx in reversed(range(0, column_idx-1)):
                             if(board[row_idx][idx] == 1):
                                 break
                             if(board[row_idx][idx] == 2):
                                 possible_points += 1
                             if(board[row_idx][idx] == 0):
-                                all_legal_moves.append([row_idx,idx,possible_points])
-                                print('all legal moves are:',all_legal_moves)
+                                all_legal_moves.append([row_idx, idx, possible_points])
+                                print('all legal moves are:', all_legal_moves)
                                 break
                             if(board[row_idx][idx] == 2):
                                 possible_points += 1
-                    
+
                     # Vertical Above
                     if(board[row_idx-1][column_idx] == 2):
-                        for idx in reversed(range(0,row_idx-1)):
+                        for idx in reversed(range(0, row_idx-1)):
                             if(board[idx][column_idx] == 1):
                                 break
                             if(board[idx][column_idx] == 2):
                                 possible_points += 1
                             if(board[idx][column_idx] == 0):
-                                all_legal_moves.append([idx,column_idx,possible_points])
-                                print('all legal moves are:',all_legal_moves)
+                                all_legal_moves.append([idx, column_idx, possible_points])
+                                self.used_directions["Vertical Above"] += 1
+                                print('all legal moves are:', all_legal_moves)
                                 break
-                    
+
                     # Vertical Below
                     if(board[row_idx+1][column_idx] == 2):
-                        for idx in range(row_idx+2,8):
+                        for idx in range(row_idx+2, 8):
                             if(board[idx][column_idx] == 1):
                                 break
                             if(board[idx][column_idx] == 2):
                                 possible_points += 1
                             if(board[idx][column_idx] == 0):
-                                all_legal_moves.append([idx,column_idx,possible_points])
-                                print('all legal moves are:',all_legal_moves)
+                                all_legal_moves.append([idx, column_idx, possible_points])
+                                print('all legal moves are:', all_legal_moves)
                                 break
-                    
-                    # Up Left Diagnol
+
+                    # Up Left diagonal
                     if(board[row_idx-1][column_idx-1] == 2):
                         temp_col_idx = column_idx-2
                         temp_row_idx = row_idx-2
@@ -78,50 +80,107 @@ class movePicker:
                             if(board[temp_row_idx][temp_col_idx] == 2):
                                 possible_points += 1
                             if(board[temp_row_idx][temp_col_idx] == 0):
-                                print('up left diagnol')
-                                all_legal_moves.append([temp_row_idx,temp_col_idx,possible_points])
+                                print('up left diagonal')
+                                self.used_directions['Up Left'] += 1
+                                all_legal_moves.append(
+                                    [temp_row_idx, temp_col_idx, possible_points])
                                 break
                             temp_col_idx -= 1
                             temp_row_idx -= 1
-                                
-                    
-                    # Up Right Diagnol
+
+                    # Up Right diagonal
                     if(board[row_idx-1][column_idx+1] == 2):
                         temp_col_idx = column_idx+2
                         temp_row_idx = row_idx-2
-                        while(temp_row_idx >= 0 and temp_col_idx > len(board)):
+                        while(temp_row_idx >= 0 and temp_col_idx < len(board)):
                             if(board[temp_row_idx][temp_col_idx] == 1):
                                 break
                             if(board[temp_row_idx][temp_col_idx] == 2):
                                 possible_points += 1
                             if(board[temp_row_idx][temp_col_idx] == 0):
-                                print('up right diagnol')
-                                all_legal_moves.append([temp_row_idx,temp_col_idx,possible_points])
+                                print('up right diagonal')
+                                self.used_directions['Up Right'] += 1
+                                all_legal_moves.append(
+                                    [temp_row_idx, temp_col_idx, possible_points])
                                 break
                             temp_col_idx += 1
                             temp_row_idx -= 1
-                    
-                    # Down Left Diagnol
-                    
-                    # Down Right Diagnol
-                    
-                    else:
-                        continue
-                    
+
+                    # Down Left diagonal
+                    if(board[row_idx+1][column_idx-1] == 2):
+                        temp_col_idx = column_idx-2
+                        temp_row_idx = row_idx+2
+                        while(temp_row_idx < len(board) and temp_col_idx >= 0):
+                            if(board[temp_row_idx][temp_col_idx] == 1):
+                                break
+                            if(board[temp_row_idx][temp_col_idx] == 2):
+                                possible_points += 1
+                            if(board[temp_row_idx][temp_col_idx] == 0):
+                                print('down left diagonal')
+                                self.used_directions['Down Left'] += 1
+                                all_legal_moves.append([temp_row_idx, temp_col_idx, possible_points])
+                                print('temp_col_idx:',temp_col_idx)
+                                print('temp_row_idx',temp_row_idx)
+                                break
+                            temp_col_idx -= 1
+                            temp_row_idx += 1
+                            
+
+                    # Down Right diagonal
+                    if(board[row_idx+1][column_idx+1] == 2):
+                        temp_col_idx = column_idx+2
+                        temp_row_idx = row_idx+2
+                        while(temp_row_idx < len(board) and temp_col_idx < len(board)):
+                            if(board[temp_row_idx][temp_col_idx] == 1):
+                                break
+                            if(board[temp_row_idx][temp_col_idx] == 2):
+                                possible_points += 1
+                            if(board[temp_row_idx][temp_col_idx] == 0):
+                                print('down right diagonal')
+                                self.used_directions['Down Right'] += 1
+                                all_legal_moves.append([temp_row_idx, temp_col_idx, possible_points])
+                                break
+                            temp_col_idx += 1
+                            temp_row_idx += 1
+
+        print(self.used_directions)
         if(len(all_legal_moves) == 0):
-            return 
+            temp = []
+            for i in range(len(board)):
+                for j in range(len(board[i])):
+                    if(board[i][j] == 0):
+                        if(board[row_idx][column_idx+1] == 2 or
+                            board[row_idx][column_idx-1] == 2 or
+                            board[row_idx-1][column_idx] == 2 or
+                            board[row_idx+1][column_idx] == 2 or
+                            board[row_idx-1][column_idx-1] == 2 or
+                            board[row_idx-1][column_idx+1] == 2 or
+                            board[row_idx+1][column_idx-1] == 2 or
+                            board[row_idx+1][column_idx+1] == 2):
+                            temp.append([board[i][j]])
+                        if(temp[i][0] == 0 and temp[i][1] == 0 or temp[i][0] == 0 and temp[i][1] == 7 or temp[i][0] == 7 and temp[i][1] == 0 or temp[i][0] == 7 and temp[i][1] == 7):
+                            return [[temp[i][0],temp[i][1]]]
+            
+            random_number = rd.randint(0,len(temp))
+            return [temp[random_number]]
+                    
         return all_legal_moves
-    
+
     @staticmethod
     def get_best_move(legal_moves):
+        print('LENGTH OF LEGAL MOVES',len(legal_moves))
         best_move = []
         max_value = 0
         max_index = 0
         for i in range(len(legal_moves)):
+            if(legal_moves[i][0] == 0 and legal_moves[i][1] == 0 or legal_moves[i][0] == 0 and legal_moves[i][1] == 7 or legal_moves[i][0] == 7 and legal_moves[i][1] == 0 or legal_moves[i][0] == 7 and legal_moves[i][1] == 7):
+                return [legal_moves[i][0],legal_moves[i][1]]
+        
+        for i in range(len(legal_moves)):
             if(legal_moves[i][2] > max_value):
                 max_value = legal_moves[i][2]
                 max_index = i
-        
+
         best_move.append(legal_moves[max_index][0])
         best_move.append(legal_moves[max_index][1])
         return best_move
--- tests/movePicker_test.py
+++ tests/movePicker_test.py
@@ -6,7 +6,14 @@ from movePicker import movePicker
 
 class movePicker_get_next_move_test(unittest.TestCase):
     def test_able_to_call(self):
-        board = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 2, 0, 0, 0],
+                 [0, 0, 0, 2, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
         mp = movePicker()
         result = mp.get_next_move(board)
 
@@ -18,23 +25,127 @@ class movePicker_get_all_legal_moves_test(unittest.TestCase):
     
     def test_has_get_all_legal_moves_function(self):
         
-        board = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 2, 0, 0, 0],
+                 [0, 0, 0, 2, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
         self._mp.get_all_legal_moves(board)
 
     def test_able_to_pass_args_to_get_all_legal_moves(self):
         
-        board = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
-        
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 2, 0, 0, 0],
+                 [0, 0, 0, 2, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
         self._mp.get_all_legal_moves(board)
     
-    def test_get_all_legal_moves_returns_legal_moves_start_of_game_non_diagnol(self):
-        board = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 2, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
-        result = self._mp.get_all_legal_moves(board)
-        self.assertListEqual(result,[[2,4],[3,5],[4,2],[5,3]])
+    # Vertical/Horizontal Tests
+    
+    def test_vertical_above_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 2, 2, 2, 0, 0],
+                 [0, 0, 0, 1, 0, 2, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[3,5])
+    
+    def test_vertical_below_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 2, 2, 2, 0, 0],
+                 [0, 0, 0, 1, 0, 2, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[6,5])
         
-    def test_able_to_finish_game(self):
-        self.assertTrue(False)
-
+    def test_horizontal_right_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 2, 2, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[4,5])
+    
+    def test_horizontal_left_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 2, 0, 0, 0],
+                 [0, 0, 2, 2, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[4,1])
+    
+    # Diagonal Tests
+    def test_up_right_diagonal_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 2, 0],
+                 [0, 0, 0, 0, 0, 2, 0, 0],
+                 [0, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 2, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[1,7])
+        
+    def test_up_left_diagonal_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 2, 0, 0, 0, 0],
+                 [0, 2, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 2, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[1,0])
+    
+    def test_down_right_diagonal_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 2, 0, 0, 0],
+                 [0, 0, 2, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 2, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 2, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[7,6])
+        
+    def test_down_left_diagonal_two_spaces(self):
+        board = [[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 2, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0]]
+        result = self._mp.get_next_move(board)
+        self.assertListEqual(result,[5,1])
+        
     
     
 if __name__ == '__main__':
